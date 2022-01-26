b3551e13c66588431ffa9a04a841c34a0dec89ac
Noah-Blevins <noah.james.blevins@gmail.com>
2022-01-26 15:16:28 -0700

    Eric's first update to Director

--- game/director.py
+++ game/director.py
@@ -1,11 +1,30 @@
-import cards
-
+from game.cards import Cards
 class Director:
     def __init__(self):
-        score = 300
-        current_card = ""
-        playing = True
+        """Atributes:
+        - score - will be updated whether the player wind or lose the round.
+        - current_card - will display a random card after the player enters high or lower.
+        - playing (boolean) - whether or not the game is being played."""
+        self.score = 300
+        self.current_card = ""
+        self.playing = True
+    def start_game(self):
+        """Starts the game by running the main game loop.
         
+        """
+        while self.playing:
+            """Display the first card
+            Get the input from the user
+            output the next card
+            display player's score
+            Ask if the user wants to play again.
+            """
+            self.display_card()
+            self.get_input()
+            self.do_updates()
+            self.give_output()
+    def display_card(self):
+        pass
     def get_input():
         pass
     
@@ -14,4 +33,3 @@ class Director:
     
     def give_output():
         pass
-        
